50ba61b5d2c7174eda5d0746b3bcf35ab3284d7c
Brad <brad@bradeagle.com>
2024-02-18 13:57:03 -0700

    Add files via upload

---
+++ resize.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+mkdir "resized"
+
+for file in converted/*.mp4*
+do
+	echo $file
+	file=$(basename "${file%.*}")
+	ffmpeg -i "converted/$file.mp4" -vf "scale=1280:720" -c:a copy -b:a 64k "resized/${file}_144.mp4"
+	ffmpeg -i "converted/$file.mp4" -vf "scale=1280:720" -c:a copy -b:a 64k "resized/${file}_240.mp4"
+	ffmpeg -i "converted/$file.mp4" -vf "scale=1280:720" -c:a copy -b:a 64k "resized/${file}_360.mp4"
+	ffmpeg -i "converted/$file.mp4" -vf "scale=1280:720" -c:a copy -b:a 64k "resized/${file}_480.mp4"
+	ffmpeg -i "converted/$file.mp4" -vf "scale=1280:720" -c:a copy -b:a 128k "resized/${file}_720.mp4"
+done
+
